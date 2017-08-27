370e898fa91f35fc68a0f57b078f50cd23bc14c7
NoobChata <NoobChata@163.com>
2017-08-27 16:50:12 +0800

    Added view and template for detailed blog post as well as CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's5(-ma_obc42h+5zhhp^c&0^c%#=kx%&^x@crdw2=$o9+qmbc%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
